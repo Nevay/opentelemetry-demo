e391cda2d51af979dc33b0f5b7f69a61c6f9de89
Tobias Bachert <git@b-privat.de>
2023-12-27 17:12:25 +0100

    Switch to async sdk

--- src/quote/public/index.php
+++ src/quote/public/index.php
@@ -8,17 +8,11 @@ declare(strict_types=1);
 
 use DI\Bridge\Slim\Bridge;
 use DI\ContainerBuilder;
-use OpenTelemetry\API\Globals;
-use OpenTelemetry\SDK\Common\Configuration\Configuration;
-use OpenTelemetry\SDK\Common\Configuration\Variables;
-use OpenTelemetry\SDK\Logs\LoggerProviderInterface;
-use OpenTelemetry\SDK\Metrics\MeterProviderInterface;
-use OpenTelemetry\SDK\Trace\TracerProviderInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use React\EventLoop\Loop;
 use React\Http\HttpServer;
 use React\Socket\SocketServer;
 use Slim\Factory\AppFactory;
+use function Amp\trapSignal;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -52,26 +46,6 @@ $app->addBodyParsingMiddleware();
 
 // Add Error Middleware
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-Loop::get()->addSignal(SIGTERM, function() {
-    exit;
-});
-
-/* workaround for non-async batch processors */
-if (($tracerProvider = Globals::tracerProvider()) instanceof TracerProviderInterface) {
-    Loop::addPeriodicTimer(Configuration::getInt(Variables::OTEL_BSP_SCHEDULE_DELAY)/1000, function() use ($tracerProvider) {
-        $tracerProvider->forceFlush();
-    });
-}
-if (($loggerProvider = Globals::loggerProvider()) instanceof LoggerProviderInterface) {
-    Loop::addPeriodicTimer(Configuration::getInt(Variables::OTEL_BLRP_SCHEDULE_DELAY)/1000, function() use ($loggerProvider) {
-        $loggerProvider->forceFlush();
-    });
-}
-if (($meterProvider = Globals::meterProvider()) instanceof MeterProviderInterface) {
-    Loop::addPeriodicTimer(Configuration::getInt(Variables::OTEL_METRIC_EXPORT_INTERVAL)/1000, function() use ($meterProvider) {
-        $meterProvider->forceFlush();
-    });
-}
 
 $server = new HttpServer(function (ServerRequestInterface $request) use ($app) {
     $response = $app->handle($request);
@@ -103,3 +77,4 @@ $socket = new SocketServer($address);
 $server->listen($socket);
 
 echo "Listening on: {$address}" . PHP_EOL;
+trapSignal(SIGTERM);
